8151b8f2a16dfe7b4c525a219d237d4f0f8abba6
Devz-0047 <devendersingh00747@gmail.com>
2024-12-21 17:01:29 +0530

    C III: Corrected images path

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,23 +3,23 @@ function Profile() {
   return (
     <div className="mt-[9rem] ">
       <img
-        src="public\bg.png"
+        src="/bg.png"
         className="h-[151px] w-[1220px] rounded-t-2xl mx-auto"
       ></img>
       <div className="h-[151px] w-[1220px] bg-[#FFFBF2] mx-auto ">
         <div className="flex items-center justify-around">
           <div className="flex items-center gap-6">
-            <img src="./public/profile.png" className="w-[159px] h-[159px]" />
+            <img src="/profile.png" className="w-[159px] h-[159px]" />
             <div className="font-poppins">
               <div className="flex items-center gap-2">
                 <h2 className="text-xl font-medium">Dr. Bruce Willis</h2>
-                <img src="./public/tick.svg" alt="blue tick"></img>{" "}
+                <img src="/tick.svg" alt="blue tick"></img>{" "}
               </div>
               <div>
-                <p className="text-base font-medium pt-1">Gynecologist </p>
+                <p className="pt-1 text-base font-medium">Gynecologist </p>
               </div>
               <div className="flex items-center gap-3 pt-2">
-                <p className="font-semibold text-base">4.2</p>
+                <p className="text-base font-semibold">4.2</p>
                 <div className="text-xl text-[#F79624] flex items-center">
                   <IoMdStar />
                   <IoMdStar />
@@ -30,20 +30,20 @@ function Profile() {
             </div>
           </div>
           <div className="flex items-center gap-16">
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
               <p className="font-medium">Followers</p>
-              <p className="font-semibold text-xl">850</p>
+              <p className="text-xl font-semibold">850</p>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
               <p className="font-medium">Following</p>
-              <p className="font-semibold text-xl">18K</p>
+              <p className="text-xl font-semibold">18K</p>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
               <p className="font-medium">Posts</p>
-              <p className="font-semibold text-xl">250</p>
+              <p className="text-xl font-semibold">250</p>
             </div>
           </div>
-          <button className="bg-highlight text-white py-3 px-8 text-xl font-inter font-medium rounded-lg">
+          <button className="px-8 py-3 text-xl font-medium text-white rounded-lg bg-highlight font-inter">
             Book an Appointment
           </button>
         </div>
